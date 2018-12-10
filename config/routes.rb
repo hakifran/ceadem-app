1220c314338a904cdd0c0ab62e37f9eeb4494d0a
Franck <fhakizimana@bluesquare.org>
2018-12-10 20:02:24 +0200

    generate members controller and its routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  root 'pages#index'
+  resources :members
   match '*path', to: "pages#index", via: :all
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
